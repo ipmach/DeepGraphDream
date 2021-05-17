7873a17b7289ea8c51237c12e756b695ad6d99f6
WilliamSchober <wschober@gmail.com>
2021-05-17 12:26:48 +0200

    added graph randomization

--- Datasets/Syntheticdata.py
+++ Datasets/Syntheticdata.py
@@ -4,12 +4,13 @@ from Datasets.data_abstract import DataClass
 
 class SyntheticData(DataClass):
 
-    def __init__(self, num_samples, permute=True):
+    def __init__(self, num_samples, mutate=True, permute_node_idx=True):
         """
         Datasets class for Synthetic datasets
         :param num_samples: size dataset
-        :param permute: do permutation
+        :param mutate: slightly randomize edges
+        :param permute_node_idx: permute node indices
         """
         self.generator = SyntheticGenerator()
-        dataset = self.generator.generate(num_samples=num_samples, permute=permute)
+        dataset = self.generator.generate(num_samples=num_samples, mutate=mutate, permute_node_idx=permute_node_idx)
         super(SyntheticData, self).__init__(dataset)
