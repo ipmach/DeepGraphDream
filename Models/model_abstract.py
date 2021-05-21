bba8ccbf90fd8f3aac36558a8bea41c03f25031a
ipmach <andre.gg96@gmail.com>
2021-05-21 12:05:32 +0200

    edge_weigths inside data

--- Models/model_abstract.py
+++ Models/model_abstract.py
@@ -38,36 +38,45 @@ class Model(torch.nn.Module):
         """
         self.load_state_dict(torch.load(path))
 
-    def train_step(self, train_loader):
+    def train_step(self, train_loader, use_weights=False):
         """
         Train step (redefine if model work different)
         :param train_loader: train loader
+        :param use_weights: use edge weights
         :return: None
         """
         self.train()
         for data in train_loader:  # Iterate in batches over the training dataset.
-            out = self(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
+            if use_weights:
+                out = self(data.x, data.edge_index, data.batch, data.edge_weights)
+            else:
+                out = self(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
             loss = self.criterion(out, data.y)  # Compute the loss.
             loss.backward()  # Derive gradients.
             self.optimizer.step()  # Update parameters based on gradients.
             self.optimizer.zero_grad()  # Clear gradients.
 
-    def test_step(self, test_loader):
+    def test_step(self, test_loader, use_weights=False):
         """
         Test step (redefine if model work different)
         :param test_loader: test loader
+        :param use_weights: use edge weights
         :return: accuracy
         """
         self.eval()
         correct = 0
         for data in test_loader:  # Iterate in batches over the training/test dataset.
-            out = self(data.x, data.edge_index, data.batch)
+            if use_weights:
+                out = self(data.x, data.edge_index, data.batch, data.edge_weights)
+            else:
+                out = self(data.x, data.edge_index, data.batch)
             pred = out.argmax(dim=1)  # Use the class with highest probability.
             correct += int((pred == data.y).sum())  # Check against ground-truth labels.
         return correct / len(test_loader.dataset)  # Derive ratio of correct predictions.
 
     def do_train(self, num_epochs, train_loader, test_loader,
-                 learning_rate=0.01, visualize=True, pause_plt=0.4):
+                 learning_rate=0.01, visualize=True, pause_plt=0.4,
+                 use_weights=False):
         """
         Do training
         :param num_epochs: number of epochs to train
@@ -76,6 +85,7 @@ class Model(torch.nn.Module):
         :param learning_rate: learning rate
         :param visualize: visualize plot each epoch
         :param pause_plt: time to wait from visualization (train is slower)
+        :param use_weights: use edge weights
         :return: train_acc, test_acc
         """
         self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
@@ -85,8 +95,10 @@ class Model(torch.nn.Module):
         test_acc = []
         for epoch in range(num_epochs):
             self.train_step(train_loader)
-            train_acc.append(self.test_step(train_loader))
-            test_acc.append(self.test_step(test_loader))
+            train_acc.append(self.test_step(train_loader,
+                             use_weights=use_weights))
+            test_acc.append(self.test_step(test_loader,
+                            use_weights=use_weights))
             if visualize:
                 display.clear_output()
                 plt.figure(1)
