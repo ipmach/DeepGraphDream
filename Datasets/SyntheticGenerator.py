1b97ccd86834847b32f8a62f4e1219055c4ea0a5
WilliamSchober <wschober@gmail.com>
2021-05-21 04:15:26 +0200

    initial add of edge_weights

--- Datasets/SyntheticGenerator.py
+++ Datasets/SyntheticGenerator.py
@@ -11,41 +11,42 @@ class SyntheticGenerator:
 
         self.num_nodes = 9  # all graphs have exactly 9 nodes
         self.classes = 2  # Number of classes
+        self.edge_weight_list = []  # optional biased edge weights (list of lists)
         low = 0.01  # probability of generating false edge
         high = 0.99  # probability of generating true edge
 
         # define base graph shapes
         # circle graph (shaped like O)
-        self.circleAdj = np.array([[low,high,low,low,low,low,low,low,high],
-                                     [0,low,high,low,low,low,low,low,low],
-                                     [0,0,low,high,low,low,low,low,low],
-                                     [0,0,0,low,high,low,low,low,low],
-                                     [0,0,0,0,low,high,low,low,low],
-                                     [0,0,0,0,0,low,high,low,low],
-                                     [0,0,0,0,0,0,low,high,low],
-                                     [0,0,0,0,0,0,0,low,high],
-                                     [0,0,0,0,0,0,0,0,low]])
+        self.circleAdj = np.array([[0,high,low,low,low,low,low,low,high],
+                                   [0,0,high,low,low,low,low,low,low],
+                                   [0,0,0,high,low,low,low,low,low],
+                                   [0,0,0,0,high,low,low,low,low],
+                                   [0,0,0,0,0,high,low,low,low],
+                                   [0,0,0,0,0,0,high,low,low],
+                                   [0,0,0,0,0,0,0,high,low],
+                                   [0,0,0,0,0,0,0,0,high],
+                                   [0,0,0,0,0,0,0,0,0]])
 
         # cross graph (shaped like X)
-        self.crossAdj = np.array([[low,high,low,high,low,high,low,high,low],
-                                  [0,low,high,low,low,low,low,low,low],
-                                  [0,0,low,low,low,low,low,low,low],
-                                  [0,0,0,low,high,low,low,low,low],
-                                  [0,0,0,0,low,low,low,low,low],
-                                  [0,0,0,0,0,low,high,low,low],
-                                  [0,0,0,0,0,0,low,low,low],
-                                  [0,0,0,0,0,0,0,low,high],
-                                  [0,0,0,0,0,0,0,0,low]])
+        self.crossAdj = np.array([[0,high,low,high,low,high,low,high,low],
+                                  [0,0,high,low,low,low,low,low,low],
+                                  [0,0,0,low,low,low,low,low,low],
+                                  [0,0,0,0,high,low,low,low,low],
+                                  [0,0,0,0,0,low,low,low,low],
+                                  [0,0,0,0,0,0,high,low,low],
+                                  [0,0,0,0,0,0,0,low,low],
+                                  [0,0,0,0,0,0,0,0,high],
+                                  [0,0,0,0,0,0,0,0,0]])
         # snowflake graph (shaped like *, currently unused)
-        self.snowflakeAdj = np.array([[low,high,high,high,high,high,high,high,high],
-                                      [0,low,low,low,low,low,low,low,low],
+        self.snowflakeAdj = np.array([[0,high,high,high,high,high,high,high,high],
                                       [0,0,low,low,low,low,low,low,low],
                                       [0,0,0,low,low,low,low,low,low],
                                       [0,0,0,0,low,low,low,low,low],
                                       [0,0,0,0,0,low,low,low,low],
                                       [0,0,0,0,0,0,low,low,low],
                                       [0,0,0,0,0,0,0,low,low],
-                                      [0,0,0,0,0,0,0,0,low]])
+                                      [0,0,0,0,0,0,0,0,low],
+                                      [0,0,0,0,0,0,0,0,0]])
         # define labels
         self.circle_label = torch.tensor([0])
         self.cross_label = torch.tensor([1])
@@ -75,7 +76,7 @@ class SyntheticGenerator:
         """
         return np.transpose(P) @ adj @ P
 
-    def generate_graphs(self, base_graph, label, num_samples, mutate):
+    def generate_graphs(self, base_graph, label, num_samples, mutate, edge_weights):
         """
         Generate a graph
         :param base_graph: topology of the graph
@@ -93,12 +94,35 @@ class SyntheticGenerator:
             if not mutate:
               edge_index, _ = dense_to_sparse(torch.tensor(adj))
             else:
-              edge_index, _ = dense_to_sparse(torch.bernoulli(torch.tensor(adj)))  # adjacency list
+              mutated_sample = torch.bernoulli(torch.tensor(adj))
+              edge_index, _ = dense_to_sparse(mutated_sample)  # adjacency list
             graph = data.Data(x=torch.rand(self.num_nodes, self.num_nodes),
                               edge_index=edge_index,
                               edge_attr=self.identity,
                               y=label)
             self.synthetic_dataset.append(graph)
+            if edge_weights:
+              weights = 0.1*np.ones(len(edge_index.T))
+              intended_edges = torch.tensor(np.copy(np.round(base_graph)))
+              intended_index, _ = dense_to_sparse(intended_edges)
+              #print(intended_index)
+
+              for i,edge in enumerate(edge_index.T):
+                #print("edge",edge.tolist())
+                edge_found = False
+                for intended_edge in intended_index.T:
+                  #print(intended_edge.tolist())
+                  if edge.tolist() == intended_edge.tolist():
+                    #print("true")
+                    edge_found = True
+                    break
+                if edge_found:
+                  weights[i] = 1.
+
+              self.edge_weight_list.append(weights)
+
+
+
 
     def generate(self, num_samples=5000, mutate=True, permute_node_idx=True, edge_weights=False):
         """
@@ -117,23 +141,19 @@ class SyntheticGenerator:
             for i in range(self.num_nodes):
                 if i != 0:
                     circle_adj = self.permute(circle_adj, self.cyclic)
-                self.generate_graphs(circle_adj, self.circle_label, int(num_samples / (2 * self.num_nodes)), mutate)
+                self.generate_graphs(circle_adj, self.circle_label, int(num_samples / (2 * self.num_nodes)), mutate, edge_weights)
 
             cross_adj = np.copy(self.crossAdj)
             for i in range(self.num_nodes):
                 if i != 0:
                     cross_adj = self.permute(cross_adj, self.cyclic)
-                self.generate_graphs(cross_adj, self.cross_label, int(num_samples / (2 * self.num_nodes)), mutate)
+                self.generate_graphs(cross_adj, self.cross_label, int(num_samples / (2 * self.num_nodes)), mutate, edge_weights)
 
         else:
-            self.generate_graphs(self.crossAdj, self.cross_label, int(num_samples / 2), mutate)
-            self.generate_graphs(self.circleAdj, self.snowflake_label, int(num_samples / 2), mutate)
+            self.generate_graphs(self.crossAdj, self.cross_label, int(num_samples / 2), mutate, edge_weights)
+            self.generate_graphs(self.circleAdj, self.snowflake_label, int(num_samples / 2), mutate, edge_weights)
 
-        edge_weight_list = None
-        #if edge_weights:
-        #    do
-
-        return WrapperSynthetic(self.synthetic_dataset, self.num_nodes, self.classes, edge_weight_list)
+        return WrapperSynthetic(self.synthetic_dataset, self.num_nodes, self.classes, self.edge_weight_list)
 
 
 class WrapperSynthetic:
